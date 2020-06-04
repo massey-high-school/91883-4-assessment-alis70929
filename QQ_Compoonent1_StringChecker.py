4378cc1f02346f936c5ae2b58dad35268786bbb5
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-06-05 10:55:09 +1200

    Update QQ_Compoonent1_StringChecker.py

--- QQ_Compoonent1_StringChecker.py
+++ QQ_Compoonent1_StringChecker.py
@@ -1,1 +1,11 @@
 # Quiz Quest Component 1 String Checker
+
+# To Do
+# - Get string input from user
+# - Check if it is valid in the given scenario
+
+def StringCheck(question):
+
+    response = input(question)
+
+    if response.lower() == "Yes"
