5acf9bb6ffaae7fc62d414270d10225c66e2ec53
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-06-10 12:37:35 +1200

    Given component 1 String checker comments

--- QQ_Compoonent1_StringChecker.py
+++ QQ_Compoonent1_StringChecker.py
@@ -4,32 +4,43 @@
 # - Get string input from user
 # - Check if it is valid in the given scenario
 
+# The Function that checks if a string is valid for yes/no
 def string_check(question, yes_response, no_response):
 
     valid = False
     while not valid:
+        # Asks User the question
         response = input(question).lower()
-
+        # Checks if response is yes or y
         if response == "yes" or response == "y":
+            # Displays feedback to user
             print(yes_response)
             print()
+            # Returns answer as yes to the main code
             return "yes"
+        # Checks if response is no or n
         elif response == "no" or response == "n":
+            # Displays feedback to user
             print(no_response)
             print()
+            # Returns answer as no to the main code
             return "no"
+        # If they entered anything other than the above
         else:
+            # Tell the user to enter yes or no
             print("Please enter Yes/No")
 
 
 # Main Routine
+# Loop for testing purposes
 loop = True
 while loop:
+    # Asks user if they want to do all 12 times tables at once and puts appropiate feedback into the string checker function
     times_table_all = string_check("Would you like to do all 12 times tables at once?",
                                    "You have chosen to practice all times tables at once",
                                    "You have chosen to do a specific times tables")
 
-
+    # Asks if they want to answer questions continously and puts appropiate feedback into the string checker function
     continuous = string_check("Would you like to answer questions continuously?",
                               "You have chosen to do questions continuously",
                               "You have chosen to do a specific amount of rounds")
--- QQ_Compoonent1_StringCheckerV2.py
+++ QQ_Compoonent1_StringCheckerV2.py
@@ -7,15 +7,20 @@
 
 # This function was made by Mrs Gottschalk
 def string_check(question, to_check):
-
+    # Loops until a valid answer is given
     valid = False
     while valid == False:
+        # Asks question
         response = input(question).lower()
 
         for item in to_check:
+            # If the users  response is in the list given(to_check)
             if response == item:
+                # return the response to the main code
                 return response
+            # If the users response is the first letter of one of the items in to_check
             elif response == item[0]:
+                # return the whole word to the main code
                 return item
         print("Please enter Yes/No")
 
@@ -23,14 +28,20 @@ def string_check(question, to_check):
 
 # Main Routine
 
-yes_no_for_string_check = ["yes","no"]
+yes_no_for_string_check = ["yes","no"] # This is a list to enter into the string checker function
+
+# Ask user question and check if answer is yes or no
 times_table_all = string_check("Would you like to do all 12 times tables at once?", yes_no_for_string_check)
+# Gives Appropiate feedback based on users response
 if times_table_all == "yes":
     print("You have chosen to practice all times tables at once")
 else:
     print("You have chosen to do a specific times tables")
 print()
+
+# Ask user question and check if answer is yes or no
 continuous = string_check("Would you like to answer questions continuously?", yes_no_for_string_check)
+# Gives Appropiate feedback based on users response
 if continuous == "yes":
     print("You have chosen to do questions continuously")
 else:
