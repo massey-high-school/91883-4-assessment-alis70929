74bca3a2f945efc8732975689824a89279bbd858
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-06-15 09:55:30 +1200

    started Component 4

---
+++ QQ_Component4_Comparison.py
@@ -0,0 +1,6 @@
+# Quiz Quest Component 4 Comparing Users answer to the actual answer
+
+# To Do
+# - Compare users answer to actual answer, Give appropiate feedback
+#  - If the user gets it right congratulate them
+#  - If they get it wrong tell them they are wrong and what the right answer is.
