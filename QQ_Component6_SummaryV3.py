b5aca2a52a05038fe20ec26657d9e231018c8f54
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-07-02 12:13:27 +1200

    Figured out how to centre align text when manually setting the amount of space it takes up

---
+++ QQ_Component6_SummaryV3.py
@@ -0,0 +1,32 @@
+# Quiz Quest Component 5 Summary V3 I figured out how to centre align
+
+# To Do
+# - Store the generated questions, the users answer, the actual answer and whether or not the user got it right
+# - Display the stored data from each round in a nice table, without it making the columns out of shape.
+
+questions = ["2 x 3", "3 x 6","12 x 12"]  # Stores each question
+actual_answers = [6 , 18, 144]  # Stores the correct answer for each question
+users_answers = [9 , 18, 144]  # Stores the users answer for each question
+correct_incorrect = ["Correct", "Incorrect", "Correct"]  # Stores wheter or not they are correct
+
+listcount = 0 # To count which questions summary needs to be generated
+print("Question  #|Question |Users Answer|Correct Answer|Correct?  |")# Displays the top of the summary table
+
+
+for item in actual_answers: # loops until there is no more question summary to display
+    # Generates a row that displays a summary for the question listcount is on, with even
+    print("Question {1:>{0}}| {3:^{2}} | {5:^{4}} | {7:^{6}} |{9:<{8}}|".format(2,listcount + 1 ,
+                                                                                7 ,questions[listcount] ,
+                                                                                10 ,users_answers[listcount],
+                                                                                12, actual_answers[listcount],
+                                                                                10, correct_incorrect[listcount]))
+
+
+    listcount +=1 # generate the next question summary row
+
+# results
+print()
+# How many correct as a fraction
+print("{}/{} questions were answered incorrectly".format(correct_incorrect.count("Incorrect"), len(correct_incorrect)))
+# How many incorrect as a fraction
+print("{}/{} questions were answered correctly".format(correct_incorrect.count("Correct"),len(correct_incorrect)))
--- Testing.py
+++ Testing.py
@@ -1,6 +1,15 @@
-length = 15
+# Testing for different components
 
 
-string = "hello"
+# Testing for summary component
+# - I was trying to figure out a way to make it so the formatting wouldn't screw with the column walls
+# - I figured out the way to do this was to manually set the character length of the area before putting the string into it.
+# - Once the string was put in it would take up a certain amount of charcters instead of just adding on
+length = 15  # The character length that i wish to manually set
 
-print('{{0}}'.format(length, string))
+
+string = "hello" # String to be displayed
+# Left aligned
+print('{1:>{0}}'.format(length, string))
+# Right Aligned
+print('{1:^{0}}'.format(length, string))
